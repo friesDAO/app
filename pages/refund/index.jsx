29a1b99bcf28a14e37ca995d6c224e64898b7c52
Slip <72151847+slip1244@users.noreply.github.com>
2022-04-29 10:53:25 -0500

    mobile refund page

--- pages/refund/index.jsx
+++ pages/refund/index.jsx
@@ -138,15 +138,22 @@ const Refund = () => {
 
 					.refund-grid {
 						grid-template-columns: 1fr;
-						grid-template-rows: repeat(3, 1fr);
+						grid-template-rows: repeat(4, 1fr);
+						height: 600px
 					}
 
 					.info {
-						grid-area: 1 / 1 / 2 / 2;
+						grid-area: 1 / 1 / 3 / 2;
 					}
 	
 					.refund {
-						grid-area: 2 / 1 / 4 / 2;
+						grid-area: 3 / 1 / 5 / 2;
+					}
+				}
+
+				@media only screen and (max-width: 470px) {
+					.refund-container {
+						font-size: 75%;
 					}
 				}
 			`}</style>
